83b5c9a04dd461941f3c6275b2693c6ab1316e90
Sumon Ahmed <csesumon8@gmail.com>
2022-06-23 08:21:34 +0600

    Add work method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
-
+    public static void work(){
+        System.out.println("This is work method");
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
+         work();
     }
 }
